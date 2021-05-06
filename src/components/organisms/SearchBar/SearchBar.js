a814de48d4b4bfe673db3507e50d421618f5f93a
Daria Kawecka <dusiek2806@gmail.com>
2021-05-07 00:30:50 +0200

    [Feature/search] Add search logic with redux (#4)
    * Add search component
    
    * Add search icon to input
    
    * Add RWD to component
    
    * Organize folders and files according to atomic design
    
    * Add search logic with redux

--- src/components/organisms/SearchBar/SearchBar.js
+++ src/components/organisms/SearchBar/SearchBar.js
@@ -3,20 +3,30 @@ import React, { useState } from 'react';
 import { Button } from 'components/atoms/Button/Button';
 import { StyledInput } from 'components/atoms/StyledInput/StyledInput';
 import { SearchContainer, SearchIcon } from './SearchBar.styles';
+//redux:
+import { GetArtistInfo } from 'actions/artistActions';
+import { useDispatch } from 'react-redux';
 
 const SearchBar = () => {
   const [inputText, setInputText] = useState('');
+  const dispatch = useDispatch();
 
   const handleInputChange = (e) => {
     setInputText(e.target.value);
   };
+  const handleClick = () => {
+    dispatch(GetArtistInfo(inputText));
+    setInputText('');
+  };
 
   return (
-    <SearchContainer>
-      <SearchIcon />
-      <StyledInput type="text" onChange={handleInputChange} placeholder="Search artists or albums" />
-      <Button>Search</Button>
-    </SearchContainer>
+    <>
+      <SearchContainer>
+        <SearchIcon />
+        <StyledInput type="text" onChange={handleInputChange} value={inputText} placeholder="Search artists or albums" />
+        <Button onClick={handleClick}>Search</Button>
+      </SearchContainer>
+    </>
   );
 };
 
