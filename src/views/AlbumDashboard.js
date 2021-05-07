4b7123c09438ddabe0c190351a37d46441fdf4bb
daria-kawecka <dusiek2806@gmail.com>
2021-05-07 17:35:33 +0200

    Add react-router

---
+++ src/views/AlbumDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AlbumDashboard = () => {
+  return (
+    <div>
+      AlbumInfo
+      <h1>Elooo</h1>
+    </div>
+  );
+};
+
+export default AlbumDashboard;
---
+++ src/views/Root.js
@@ -0,0 +1,34 @@
+import React from 'react';
+//redux provider:
+import { Provider } from 'react-redux';
+import store from 'store/store';
+//styled-componets provider:
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from 'assets/styles/GlobalStyles';
+import { theme } from 'assets/styles/theme';
+//components:
+import MainTemplate from '../components/templates/MainTemplate/MainTemplate';
+import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import AlbumDashboard from './AlbumDashboard';
+const Root = () => {
+  return (
+    <Router>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Switch>
+            <Route path="/album/:id">
+              <AlbumDashboard />
+            </Route>
+            <Route path="/">
+              <MainTemplate />
+            </Route>
+            {/*<Redirect to={'/'} />*/}
+          </Switch>
+        </ThemeProvider>
+      </Provider>
+    </Router>
+  );
+};
+
+export default Root;
