171c4f0bc9b0c95a81ba68c586a6abd8dc64c844
daria-kawecka <dusiek2806@gmail.com>
2021-05-07 16:46:50 +0200

    Update rootReducer with albumReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import { artistReducer } from './artistReducer';
+import { albumReducer } from './albumReducer';
 
 export const rootReducer = combineReducers({
   artistInfo: artistReducer,
+  albumInfo: albumReducer,
 });
